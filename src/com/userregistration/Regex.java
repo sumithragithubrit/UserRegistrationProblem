1720c267cbc4e2238906694a594d09eb2f4c850f
sumi <sumipavib1302@gmail.com>
2023-01-28 16:49:20 +0530

    Added:FirstNameValid

--- src/com/userregistration/Regex.java
+++ src/com/userregistration/Regex.java
@@ -1,5 +1,18 @@
 package com.userregistration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regex {
+
+    public  void validFirstName() {
+         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+        Matcher matcher = pattern.matcher("pattern");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+        
+    }     
 
 }
--- src/com/userregistration/RegexMain.java
+++ src/com/userregistration/RegexMain.java
@@ -1,5 +1,10 @@
 package com.userregistration;
 
 public class RegexMain {
-
+	
+	 public static void main(String args[]){
+	        Regex regex = new Regex();
+	        regex.validFirstName();
+	        
+	    }
 }
