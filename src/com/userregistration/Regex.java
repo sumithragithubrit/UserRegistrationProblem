94d16c81085198791ddadc8f6decd20c4afa6931
sumi <sumipavib1302@gmail.com>
2023-01-28 21:43:37 +0530

    Added:Contact number validation

--- src/com/userregistration/Regex.java
+++ src/com/userregistration/Regex.java
@@ -4,16 +4,19 @@ import java.util.regex.Pattern;
 
 public class Regex {
 
+	//firstname validation
     public  void validFirstName() {
          Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
         Matcher matcher = pattern.matcher("Sumithra");
-        if (matcher.matches()) {
+        if (matcher.matches()){
             System.out.println("valid");
         }else {
             System.out.println("invalid");
         }
         
     }   
+    
+    //lastname validation
     public  void validLastName() {
         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
         Matcher matcher = pattern.matcher("Buddareddy");
@@ -24,15 +27,26 @@ public class Regex {
         }
     }
     
+    //email validation
     public void  validEmail() {
-
         Pattern pattern = Pattern.compile("^(.+)@(.+)$");
-        Matcher matcher = pattern.matcher("sumipavib1302@gmail.com");
-        if (matcher.matches()) {
+        Matcher matcher = pattern.matcher("sumpavib1302@gmail.com");
+        if (matcher.matches()){
             System.out.println("true");
         }else {
             System.out.println("false");
         }
     }
+    
+   //contact number validation
+    public  void validContactNumber(){
+            Pattern pattern = Pattern.compile("^[0-9]{1,2}\\s{1}[0-9]{10}$");
+            Matcher matcher = pattern.matcher("91 4567890987");
+            if (matcher.matches()){
+                System.out.println("valid");
+            }else {
+                System.out.println("invalid");
+            }
+        }
 
 }
