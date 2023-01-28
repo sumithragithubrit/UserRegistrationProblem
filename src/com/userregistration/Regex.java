2658e36d486e87f8d6fc2a00f518785aea07c539
sumi <sumipavib1302@gmail.com>
2023-01-28 17:06:50 +0530

    Added:LastNameValidation

--- src/com/userregistration/Regex.java
+++ src/com/userregistration/Regex.java
@@ -6,13 +6,22 @@ public class Regex {
 
     public  void validFirstName() {
          Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
-        Matcher matcher = pattern.matcher("pattern");
+        Matcher matcher = pattern.matcher("Sumithra");
         if (matcher.matches()){
             System.out.println("valid");
         }else {
             System.out.println("invalid");
         }
         
-    }     
+    }   
+    public  void validLastName() {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+        Matcher matcher = pattern.matcher("buddareddy");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
 
 }
--- src/com/userregistration/RegexMain.java
+++ src/com/userregistration/RegexMain.java
@@ -1,10 +1,13 @@
 package com.userregistration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegexMain {
 	
 	 public static void main(String args[]){
 	        Regex regex = new Regex();
 	        regex.validFirstName();
-	        
+	        regex.validLastName();
 	    }
+	
 }
